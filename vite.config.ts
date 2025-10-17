857b3adc7cdc2837b2f2da00e9c8da10d856232d
Widjigo <123427281+Widjigo@users.noreply.github.com>
2025-10-17 19:07:00 +0000

    dsd

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: '.',         // or 'src' if your files are there
-  build: { outDir: 'dist' }
+  build: { outDir: 'doc' }
 })
